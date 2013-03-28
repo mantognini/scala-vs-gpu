fe9c792d2744df0e09b2ad565f265ba10fa61f90
Marco Antognini <antognini.marco@gmail.com>
2013-03-28 09:12:26 +0100

    Remove old code

--- c++/Mandelbrot/Mandelbrot/main.cpp
+++ c++/Mandelbrot/Mandelbrot/main.cpp
@@ -12,29 +12,6 @@
 #include <complex>
 #include <SFML/Graphics.hpp>
 
-//template<typename ColorType, typename ImageType>
-//void draw_Mandelbrot(ImageType& image,                                   //where to draw the image
-//                     ColorType set_color, ColorType non_set_color,       //which colors to use for set/non-set points
-//                     double cxmin, double cxmax, double cymin, double cymax,//the rect to draw in the complex plane
-//                     unsigned int max_iterations)                          //the maximum number of iterations
-//{
-//    std::size_t const ixsize = get_first_dimension(ImageType);
-//    std::size_t const iysize = get_first_dimension(ImageType);
-//    for (std::size_t ix = 0; ix < ixsize; ++ix)
-//        for (std::size_t iy = 0; iy < iysize; ++iy)
-//        {
-//            std::complex<double> c(cxmin + ix/(ixsize-1.0)*(cxmax-cxmin), cymin + iy/(iysize-1.0)*(cymax-cymin));
-//            std::complex<double> z = 0;
-//            unsigned int iterations;
-//
-//            for (iterations = 0; iterations < max_iterations && std::abs(z) < 2.0; ++iterations)
-//                z = z*z + c;
-//
-//            image[ix][iy] = (iterations == max_iterations) ? set_color : non_set_color;
-//            
-//        }
-//}
-
 namespace mb {
 
     typedef bool Color;
