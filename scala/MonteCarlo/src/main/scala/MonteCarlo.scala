2c2023250917e6ef2ef116b1bf513935fb8a3623
Marco Antognini <antognini.marco@gmail.com>
2013-03-30 15:37:48 +0100

    Add parallel impl. of Scala MC

--- scala/MonteCarlo/src/main/scala/MonteCarlo.scala
+++ scala/MonteCarlo/src/main/scala/MonteCarlo.scala
@@ -1,5 +1,6 @@
 import org.scalameter.api._
 import scala.util.{ Random }
+import scala.collection.parallel.immutable.{ ParSeq }
 
 object MonteCarlo extends PerformanceTest {
     /* configuration */
@@ -26,7 +27,7 @@ object MonteCarlo extends PerformanceTest {
         def randomPoint: (Double, Double) = (gX.nextDouble, gY.nextDouble)
 
         // Create some random point in the square
-        val points = 0 until pointCount map { _ => randomPoint }
+        val points = Seq.fill(pointCount)(randomPoint)
 
         // Count point inside the circle
         val pointInCircleCount = points count { case (x, y) => x * x + y * y <= 1 }
@@ -37,16 +38,46 @@ object MonteCarlo extends PerformanceTest {
         ratio
     }
 
-    val lowCounts = Gen.exponential("point counts")(128, 32768, 2) // From 2^7 to 2^15
-    val highCounts = Gen.range("point counts")(65536, 4194304, 131072) // From 2^16 to 2^22
+    def computeRatioParallel(pointCount: Int): Double = {
+        // Create two uniform random number generators
+        val gX = Random
+        val gY = Random
+        def randomPoint: (Double, Double) = (gX.nextDouble, gY.nextDouble)
+
+        // Create some random point in the square
+        val points = ParSeq.fill(pointCount)(randomPoint)
+
+        // Count point inside the circle
+        val pointInCircleCount = points count { case (x, y) => x * x + y * y <= 1 }
+
+        // π/4 = .785398163
+        val ratio = pointInCircleCount.toDouble / pointCount.toDouble
+
+        ratio
+    }
+
+    val lowCounts = Gen.exponential("point count")(128, 32768, 2) // From 2^7 to 2^15
+    val highCounts = Gen.range("point count")(65536, 4194304, 131072) // From 2^16 to 2^22
 
     performance of "MonteCarlo" in {
         performance of "Low Count" in {
-            using(lowCounts) in { pointCount => computeRatio(pointCount) }
+            measure method "computeRatio" in {
+                using(lowCounts) in { pointCount => computeRatio(pointCount) }
+            }
+
+            measure method "computeRatioParallel" in {
+                using(lowCounts) in { pointCount => computeRatioParallel(pointCount) }
+            }
         }
 
         performance of "High Count" in {
-            using(highCounts) in { pointCount => computeRatio(pointCount) }
+            measure method "computeRatio" in {
+                using(lowCounts) in { pointCount => computeRatio(pointCount) }
+            }
+
+            measure method "computeRatioParallel" in {
+                using(lowCounts) in { pointCount => computeRatioParallel(pointCount) }
+            }
         }
     }
 }
