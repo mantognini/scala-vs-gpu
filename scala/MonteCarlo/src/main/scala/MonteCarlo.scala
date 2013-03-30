b9728d2db726159b3960424f573fe0caad4e4864
Marco Antognini <antognini.marco@gmail.com>
2013-03-30 16:05:34 +0100

    Reduce code duplication and improve benchmark for Scala MC

--- scala/MonteCarlo/src/main/scala/MonteCarlo.scala
+++ scala/MonteCarlo/src/main/scala/MonteCarlo.scala
@@ -1,6 +1,7 @@
 import org.scalameter.api._
 import scala.util.{ Random }
 import scala.collection.parallel.immutable.{ ParSeq }
+import scala.collection.{ GenSeq }
 
 object MonteCarlo extends PerformanceTest {
     /* configuration */
@@ -20,32 +21,30 @@ object MonteCarlo extends PerformanceTest {
     )
     lazy val persistor = Persistor.None
 
-    def computeRatio(pointCount: Int): Double = {
-        // Create two uniform random number generators
-        val gX = Random
-        val gY = Random
-        def randomPoint: (Double, Double) = (gX.nextDouble, gY.nextDouble)
-
-        // Create some random point in the square
-        val points = Seq.fill(pointCount)(randomPoint)
-
-        // Count point inside the circle
-        val pointInCircleCount = points count { case (x, y) => x * x + y * y <= 1 }
-
-        // π/4 = .785398163
-        val ratio = pointInCircleCount.toDouble / pointCount.toDouble
-
-        ratio
+    trait Filler {
+        def fill[A](count: Int)(f: => A): GenSeq[A]
     }
 
-    def computeRatioParallel(pointCount: Int): Double = {
+    object SequentialFiller extends Filler {
+        def fill[A](count: Int)(f: => A): GenSeq[A] = Seq.fill[A](count)(f)
+    }
+
+    object ParallelFiller extends Filler {
+        def fill[A](count: Int)(f: => A): GenSeq[A] = ParSeq.fill[A](count)(f)
+    }
+
+    def computeRatio(pointCount: Int): Double = computeRatioGeneric(SequentialFiller)(pointCount)
+
+    def computeRatioParallel(pointCount: Int): Double = computeRatioGeneric(ParallelFiller)(pointCount)
+
+    def computeRatioGeneric[T <: Filler](filler: T)(pointCount: Int): Double = {
         // Create two uniform random number generators
         val gX = Random
         val gY = Random
         def randomPoint: (Double, Double) = (gX.nextDouble, gY.nextDouble)
 
         // Create some random point in the square
-        val points = ParSeq.fill(pointCount)(randomPoint)
+        val points = filler.fill(pointCount)(randomPoint)
 
         // Count point inside the circle
         val pointInCircleCount = points count { case (x, y) => x * x + y * y <= 1 }
@@ -57,7 +56,7 @@ object MonteCarlo extends PerformanceTest {
     }
 
     val lowCounts = Gen.exponential("point count")(128, 32768, 2) // From 2^7 to 2^15
-    val highCounts = Gen.range("point count")(65536, 4194304, 131072) // From 2^16 to 2^22
+    val highCounts = Gen.range("point count")(65536, 4194304, 524288) // From 2^16 to 2^22 in ~8 steps
 
     performance of "MonteCarlo" in {
         performance of "Low Count" in {
@@ -72,11 +71,11 @@ object MonteCarlo extends PerformanceTest {
 
         performance of "High Count" in {
             measure method "computeRatio" in {
-                using(lowCounts) in { pointCount => computeRatio(pointCount) }
+                using(highCounts) in { pointCount => computeRatio(pointCount) }
             }
 
             measure method "computeRatioParallel" in {
-                using(lowCounts) in { pointCount => computeRatioParallel(pointCount) }
+                using(highCounts) in { pointCount => computeRatioParallel(pointCount) }
             }
         }
     }
