26b25b9eac80c7ee11ecc27accecb268bbcb02b5
Marco Antognini <antognini.marco@gmail.com>
2013-03-30 14:56:39 +0100

    Edit Scala MC to benchmark low and high point count

--- scala/MonteCarlo/src/main/scala/MonteCarlo.scala
+++ scala/MonteCarlo/src/main/scala/MonteCarlo.scala
@@ -13,35 +13,42 @@ object MonteCarlo extends PerformanceTest {
         Aggregator.min,
         new Measurer.Default
     )*/
-    lazy val reporter = new LoggingReporter
+    lazy val reporter = Reporter.Composite(
+        ChartReporter(ChartFactory.XYLine()), 
+        new LoggingReporter // TODO edit this logger to display π/4 approximations
+    )
     lazy val persistor = Persistor.None
 
-    // List of results
-    var results = List.empty[(Int, Double)] /* (point count, π/4 approximation) */
+    def computeRatio(pointCount: Int): Double = {
+        // Create two uniform random number generators
+        val gX = Random
+        val gY = Random
+        def randomPoint: (Double, Double) = (gX.nextDouble, gY.nextDouble)
 
-    val counts = Gen.exponential("point counts")(128, 4194304, 2)
+        // Create some random point in the square
+        val points = 0 until pointCount map { _ => randomPoint }
 
-    performance of "Monte Carlo π/4" in {
-        using(counts) in { pointCount =>
-            // Create two uniform random number generators
-            val gX = Random
-            val gY = Random
-            def randomPoint: (Double, Double) = (gX.nextDouble, gY.nextDouble)
+        // Count point inside the circle
+        val pointInCircleCount = points count { case (x, y) => x * x + y * y <= 1 }
 
-            // Create some random point in the square
-            val points = 0 until pointCount map { _ => randomPoint }
+        // π/4 = .785398163
+        val ratio = pointInCircleCount.toDouble / pointCount.toDouble
 
-            // Count point inside the circle
-            val pointInCircleCount = points count { case (x, y) => x * x + y * y <= 1 }
-
-            // π/4 = .785398163
-            val ratio = pointInCircleCount.toDouble / pointCount.toDouble
-
-            results = (pointCount, ratio) :: results
-        }
+        ratio
     }
 
-    println("Results:\n" + results.map{ case (c, a) => c + " ~> " + a}.mkString("\n"))
+    val lowCounts = Gen.exponential("point counts")(128, 32768, 2) // From 2^7 to 2^15
+    val highCounts = Gen.range("point counts")(65536, 4194304, 131072) // From 2^16 to 2^22
+
+    performance of "MonteCarlo" in {
+        performance of "Low Count" in {
+            using(lowCounts) in { pointCount => computeRatio(pointCount) }
+        }
+
+        performance of "High Count" in {
+            using(highCounts) in { pointCount => computeRatio(pointCount) }
+        }
+    }
 }
 
 
