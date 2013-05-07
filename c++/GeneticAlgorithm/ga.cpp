26e283fcf510d81daeca6f9f203813b7bac030b0
Marco Antognini <antognini.marco@gmail.com>
2013-05-07 16:05:04 +0200

    working on ga

--- c++/GeneticAlgorithm/ga.cpp
+++ c++/GeneticAlgorithm/ga.cpp
@@ -6,67 +6,81 @@ template <typename E>
 class Population
 {
 public:
-	// Type Aliases
+    // Type Aliases
 
-	typedef typename std::function<E*()> EntityGenerator;
-	typedef typename std::vector<E*> Pop;
+    typedef typename std::function<E*()> EntityGenerator;
+    typedef typename std::vector<E*> Pop;
 
 public:
-	// Public API
+    // Public API
 
-	/*
-	 * Ctor
-	 *
-	 * @param size size of the population
-	 * @param generator Generate new Entity randomly; the ownership of those objects is transfered to this Population
-	 */
+    /*
+     * Ctor
+     *
+     * @param size size of the population
+     * @param generator Generate new Entity randomly; the ownership of those objects is transfered to this Population
+     */
     Population(unsigned int size, EntityGenerator generator)
-    : myPop(size, nullptr)
-    {
-    	for (unsigned int i = 0; i < size; ++i) {
-    		myPop[i] = generator();
-    	}
+    : myEntityGen(generator) {
+        initPop(size);
     }
 
     /// Dtor
-    ~Population()
-    {
-    	deluge();
+    ~Population() {
+        deluge();
+    }
+
+    // TODO add more methods and stuff..
+
+private:
+    // Private methods
+
+	/// Initialise the population
+    void initPop(unsigned int size) {
+        deluge();
+
+        for (unsigned int i = 0; i < size; ++i) {
+            myPop.push_back( myEntityGen() );
+        }
+    }
+
+    /// Clear the population completly
+    void deluge() {
+    	for (auto& e: myPop) {
+    		delete e;
+    		e = nullptr;
+    	}
+    	myPop.clear();
     }
 
 private:
-	// Private methods
-
-	/// Clear the population completly
-	void deluge()
-	{
-
-	}
-
-private:
-	// Data
-	Pop myPop;
-	EntityGenerator generator;
+    // Data
+    Pop myPop;
+    EntityGenerator myEntityGen;
 };
 
 
-class Entity
+// TODO implement this class
+class BooleanExpression
 {
 public:
-    Entity() {}
-    ~Entity() {}
-
     /* data */
 };
 
 int main(int, char const**)
 {
-	unsigned int const SIZE = 100;
-	auto generator = []() -> Entity* { return new Entity(); };
-	Population<Entity> pop(SIZE, generator);
+    // Initialise a population of boolean expressions
+    unsigned int const SIZE = 100;
+    auto generator = []() -> BooleanExpression* {
+    	// TODO add randomness
+        return new BooleanExpression();
+    };
+    Population<BooleanExpression> pop(SIZE, generator);
 
 
-	return 0;
+
+
+    return 0;
 }
 
 
