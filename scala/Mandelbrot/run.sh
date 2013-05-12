d1b69d05e29d3773354938e65d384e940d302365
Marco Antognini <antognini.marco@gmail.com>
2013-05-12 12:06:31 +0200

    Update run script

--- scala/Mandelbrot/run.sh
+++ scala/Mandelbrot/run.sh
@@ -7,6 +7,7 @@ mkdir -p tmp
 sbt compile
 
 javaopts="-Xmx8G -XX:+UseConcMarkSweepGC -XX:MaxPermSize=8G -XX:+UseCondCardMark -server"
+workstealingopts="-Dstep=1 -DincFreq=1 -DmaxStep=4096 -Drepeats=1 -Dstrategy=FindMax$ -Ddebug=true -Dpar=8"
 
 ## Workstealing benchmarks (with new parallel collection)
 
@@ -25,7 +26,7 @@ do
          -Dsize=$size \
          -Dthreshold=$iter \
          -Dbounds=$range \
-         -Dstep=1 -DincFreq=1 -DmaxStep=4096 -Drepeats=1 -Dstrategy=FindMax$ -Ddebug=true -Dpar=8 \
+         $workstealingopts \
          "workstealing.$target" 20 \
     | tee "data.${target}.txt"
 done
@@ -36,6 +37,7 @@ done
 
 java $javaopts \
      -cp "$HOME/.sbt/boot/scala-2.10.1/lib/scala-library.jar":"`dirname $0`/lib/workstealing_2.10-0.1.jar":"`dirname $0`/target/scala-2.10/classes/" \
+     $workstealingopts \
      "Mandelbrot" \
 | tee data.precsv
    
