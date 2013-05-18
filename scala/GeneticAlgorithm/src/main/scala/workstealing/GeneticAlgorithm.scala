9d3f1d5fae45a7ef58bdef492d252ed6d963fb98
Marco Antognini <antognini.marco@gmail.com>
2013-05-18 19:40:13 +0200

    Add run stubs

--- scala/GeneticAlgorithm/src/main/scala/workstealing/GeneticAlgorithm.scala
+++ scala/GeneticAlgorithm/src/main/scala/workstealing/GeneticAlgorithm.scala
@@ -7,6 +7,78 @@ trait Population[Entity] {
   // PUBLIC API
 
   def run(size: Int, K: Int, M: Int, N: Int, CO: Int): Entity = {
+    // Step 0.
+    // -------
+    //
+    // Make sure parameters are valid
+    val pop = ???
+
+    // Step 1 + 2.
+    // -----------
+    //
+    // Generate a population & evaluate it
+    ???
+
+    // Now sort it
+    ???
+
+    var rounds = 0;
+
+    do {
+      rounds += 1
+
+      // Step 3.
+      // -------
+      //
+      // Remove the worse K individuals
+
+      // Skipped -> replace those entities with step 5 & 6
+
+      // Step 4.
+      // -------
+      //
+      // Mutate M individuals of the population
+
+      // Choose M random individuals from the living ones, that is in range [0, size-K[
+      ???
+
+      // Step 5.
+      // -------
+      //
+      // Create CO new individuals with CrossOver
+
+      // Replace the last CO entities before the N last ones (see comment at step 3)
+      ???
+
+      // Step 6.
+      // -------
+      //
+      // Generate N new individuals randomly
+
+      // Replace the last N entities (see comment at step 3)
+      ???
+
+      // Step 7.
+      // -------
+      //
+      // Evaluate the current population
+
+      // The evaluation of new entities was already done in step 3 to 6
+      // So we only sort the population
+      ???
+
+      // Step 8.
+      // -------
+      //
+      // Goto Step 3 if the population is not stable yet
+    } while (!terminator(pop))
+
+    println("#Rounds : " + rounds)
+
+    // Step 9.
+    // -------
+    //
+    // Identify the best individual from the current population
     ???
   }
 
