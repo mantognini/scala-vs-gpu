adacfbd4f70f426270579932ebe45c0fdf157fd7
Marco Antognini <antognini.marco@gmail.com>
2013-05-26 18:50:05 +0200

    Rename some csv to precsv

--- thrust/GeneticAlgorithm/run.sh
+++ thrust/GeneticAlgorithm/run.sh
@@ -6,19 +6,19 @@ make
 
 echo "GA THRUST - CUDA # 1"
 
-./bin/ga | tee data1.cuda.csv
+./bin/ga | tee data1.cuda.precsv
 
 echo "GA THRUST - TBB # 1"
 
-./bin/ga-tbb | tee data1.tbb.csv
+./bin/ga-tbb | tee data1.tbb.precsv
 
 
 
 # echo "GA THRUST - CUDA # 2"
 
-# ./bin/ga2 | tee data2.cuda.csv
+# ./bin/ga2 | tee data2.cuda.precsv
 
 # echo "GA THRUST - TBB # 2"
 
-# ./bin/ga2-tbb | tee data2.tbb.csv
+# ./bin/ga2-tbb | tee data2.tbb.precsv
 
