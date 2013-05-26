bb9d403ba3b2f8ce21ce4c172cf28798f2a6b166
Marco Antognini <antognini.marco@gmail.com>
2013-05-26 19:29:47 +0200

    Remove also lines with 1 or 2 rounds only

--- thrust/GeneticAlgorithm/clean_precsv.sh
+++ thrust/GeneticAlgorithm/clean_precsv.sh
@@ -2,6 +2,6 @@
 
 for file in *.precsv;
 do
-	sed -e '/#/d' "$file" > "`basename -s .precsv "$file"`.csv"
+	sed -e '/#rounds = [1,2]$/,/.*/d;/#/d' "$file" > "`basename -s .precsv "$file"`.csv"
 done
 
